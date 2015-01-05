1147402c595ecd548f7fd2af1072dba962cd4311
Ran <rankorn@gmail.com>
2015-01-05 12:17:34 +0200

    Initial version

---
+++ example/src/main/java/com/foxxymobile/wearmock/example/MainActivity.java
@@ -0,0 +1,18 @@
+package com.foxxymobile.wearmock.example;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.foxxymobile.wearmock.R;
+
+/**
+ * Created by Ran on 05/01/2015.
+ */
+public class MainActivity extends ActionBarActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
